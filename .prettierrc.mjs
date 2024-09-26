9d245a094de2ac14e41c9a637974ac4163f7f0d8
yo-linux <nixon2045@gmail.com>
2024-09-25 22:56:24 -0500

    se realiza instalacion de prettier para darle formato y orden al codigo

---
+++ .prettierrc.mjs
@@ -0,0 +1,15 @@
+export default {
+    semi: true,
+    singleQuote: false,
+    tabWidth: 2,
+    useTabs: true,
+    plugins: ["prettier-plugin-astro"],
+    overrides: [
+        {
+            files:"**/*astro",
+            options: {
+                parser: "astro",
+            },
+        },
+    ],
+};
